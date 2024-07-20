019f5ff30f2a6d6d3cb86db9a142dcb42b370a80
adam hassouni <adamhassouni111@gmail.com>
2024-07-20 18:46:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@
 
 credentials = {
     'usdt': 1000,         # Amount of USDT to use for trading
-    'api': 'v2oBh1dhh3TcfLTHbtRClk0lzNBiNHwqB5DUO59BvNhPYw93TLnBn4aCqJDGRdmA', # Your Binance API key
-    'sec': 'UyWdFTuIRrLyqQ81zC1nEBN0stsQj5x2t3aBCLO94cHNqOwCjDwvLbtSbywaznpv', # Your Binance secret key
+    'api': 'API_KEY', # Your Binance API key
+    'sec': 'API_SEC', # Your Binance secret key
     'profit': 0.3,       # Target profit percentage
     'sloss': 0.05         # Stop loss percentage
 }
+#profit and sloss depends on the agressivity of the trade you want to do when the coin is listed 
